514084bd5e78e269c6ca42437a787673ed28838d
Dane Stier <DaneStier@u.northwestern.edu>
2018-06-17 14:44:50 -0500

    Other pages

---
+++ password.js
@@ -0,0 +1,21 @@
+
+
+function checkPassword() {
+	var first = document.getElementById("v1").value;
+	var second = document.getElementById("v2").value;
+	var third = document.getElementById("v3").value;
+	var fourth = document.getElementById("v4").value;
+	var fifth = document.getElementById("v5").value;
+	var sixth = document.getElementById("v6").value;
+
+	var attempt = first + second + third + fourth + fifth + sixth;
+
+	//password = 72BSA!
+
+	if (first.match('7') && second.match('2') && third.match('B') && fourth.match('S') && fifth.match('A') && sixth.match('!')) {
+		window.location = "file:///Users/danestier/Documents/Development/password/index2.html?";
+	}
+	else {
+		alert("Incorrect passcode.");
+	}
+}
